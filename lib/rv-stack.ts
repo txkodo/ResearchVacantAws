bfc386f9e4cf55e52c6b13f6abe325de5a5d4ed4
CivilTT <civiltt.ss@gmail.com>
2024-12-16 00:58:21 +0900

    ✨ API Gatewayを作成（サンプル）

--- lib/rv-stack.ts
+++ lib/rv-stack.ts
@@ -1,10 +1,24 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as apigatewayv2 from 'aws-cdk-lib/aws-apigatewayv2';
+import * as integrations from 'aws-cdk-lib/aws-apigatewayv2-integrations';
 
 export class ResearchVacantStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const apigw = new apigatewayv2.HttpApi(this, 'Apigw', {
+      // xxx/hogehoge のように，後ろに'hogehoge'のようなフォールバックを付けられるようにするのがIntegration
+      defaultIntegration: new integrations.HttpUrlIntegration(
+        'default',
+        'https://github.com',
+        {
+          method: apigatewayv2.HttpMethod.ANY,
+        }
+      ),
+    });
+
+    new cdk.CfnOutput(this, 'ApiUrl', { value: apigw.url ?? '' });
 
     // The code that defines your stack goes here
 
