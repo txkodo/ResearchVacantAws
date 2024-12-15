50e3d2e4bf50169629a6ef0c46ba9ebe5b4a6c6e
txkodo <kodo.hsmkai@gmail.com>
2024-12-16 01:21:16 +0900

    param変数をapiで受けるサンプル

--- src/api.ts
+++ src/api.ts
@@ -1,10 +1,25 @@
-import { Hono } from 'hono'
-import { handle } from 'hono/aws-lambda'
+import { Hono } from 'hono';
+import { handle } from 'hono/aws-lambda';
+import { zValidator } from '@hono/zod-validator';
+import { z } from 'zod';
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/foo', (c) => c.text('foo!!'))
+app.get(
+  '/foo/:id',
+  zValidator(
+    'param',
+    z.object({
+      id: z.string(),
+    })
+  ),
+  async (c) => {
+    return c.text(`foo!! ${c.req.param().id}`);
+  }
+);
 
-app.get('/bar', (c) => c.text('bar!!'))
+app.post('/bar', async (c) => {
+  c.text('bar!!');
+});
 
-export const handler = handle(app)
+export const handler = handle(app);
