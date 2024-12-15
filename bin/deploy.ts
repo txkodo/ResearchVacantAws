4cc3631cdff39995d6c529ee9c549fb35cdc00a8
CivilTT <civiltt.ss@gmail.com>
2024-12-16 00:39:23 +0900

    🚚 ファイル名をアプリ名に修正

--- bin/deploy.ts
+++ bin/deploy.ts
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { YoteiStack } from '../lib/yotei-stack';
+import { ResearchVacantStack } from '../lib/rv-stack';
 
 const app = new cdk.App();
-new YoteiStack(app, 'YoteiStack', {
+new ResearchVacantStack(app, 'ResearchVacantStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
